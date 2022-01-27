5e48464ec9ae0b3b5afb756d67ec590e8cdbc111
pk8442 <94054562+princegarg2808@users.noreply.github.com>
2022-01-27 14:34:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,206 @@
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+let country_list = {
+    "AED" : "AE",
+    "AFN" : "AF",
+    "XCD" : "AG",
+    "ALL" : "AL",
+    "AMD" : "AM",
+    "ANG" : "AN",
+    "AOA" : "AO",
+    "AQD" : "AQ",
+    "ARS" : "AR",
+    "AUD" : "AU",
+    "AZN" : "AZ",
+    "BAM" : "BA",
+    "BBD" : "BB",
+    "BDT" : "BD",
+    "XOF" : "BE",
+    "BGN" : "BG",
+    "BHD" : "BH",
+    "BIF" : "BI",
+    "BMD" : "BM",
+    "BND" : "BN",
+    "BOB" : "BO",
+    "BRL" : "BR",
+    "BSD" : "BS",
+    "NOK" : "BV",
+    "BWP" : "BW",
+    "BYR" : "BY",
+    "BZD" : "BZ",
+    "CAD" : "CA",
+    "CDF" : "CD",
+    "XAF" : "CF",
+    "CHF" : "CH",
+    "CLP" : "CL",
+    "CNY" : "CN",
+    "COP" : "CO",
+    "CRC" : "CR",
+    "CUP" : "CU",
+    "CVE" : "CV",
+    "CYP" : "CY",
+    "CZK" : "CZ",
+    "DJF" : "DJ",
+    "DKK" : "DK",
+    "DOP" : "DO",
+    "DZD" : "DZ",
+    "ECS" : "EC",
+    "EEK" : "EE",
+    "EGP" : "EG",
+    "ETB" : "ET",
+    "EUR" : "FR",
+    "FJD" : "FJ",
+    "FKP" : "FK",
+    "GBP" : "GB",
+    "GEL" : "GE",
+    "GGP" : "GG",
+    "GHS" : "GH",
+    "GIP" : "GI",
+    "GMD" : "GM",
+    "GNF" : "GN",
+    "GTQ" : "GT",
+    "GYD" : "GY",
+    "HKD" : "HK",
+    "HNL" : "HN",
+    "HRK" : "HR",
+    "HTG" : "HT",
+    "HUF" : "HU",
+    "IDR" : "ID",
+    "ILS" : "IL",
+    "INR" : "IN",
+    "IQD" : "IQ",
+    "IRR" : "IR",
+    "ISK" : "IS",
+    "JMD" : "JM",
+    "JOD" : "JO",
+    "JPY" : "JP",
+    "KES" : "KE",
+    "KGS" : "KG",
+    "KHR" : "KH",
+    "KMF" : "KM",
+    "KPW" : "KP",
+    "KRW" : "KR",
+    "KWD" : "KW",
+    "KYD" : "KY",
+    "KZT" : "KZ",
+    "LAK" : "LA",
+    "LBP" : "LB",
+    "LKR" : "LK",
+    "LRD" : "LR",
+    "LSL" : "LS",
+    "LTL" : "LT",
+    "LVL" : "LV",
+    "LYD" : "LY",
+    "MAD" : "MA",
+    "MDL" : "MD",
+    "MGA" : "MG",
+    "MKD" : "MK",
+    "MMK" : "MM",
+    "MNT" : "MN",
+    "MOP" : "MO",
+    "MRO" : "MR",
+    "MTL" : "MT",
+    "MUR" : "MU",
+    "MVR" : "MV",
+    "MWK" : "MW",
+    "MXN" : "MX",
+    "MYR" : "MY",
+    "MZN" : "MZ",
+    "NAD" : "NA",
+    "XPF" : "NC",
+    "NGN" : "NG",
+    "NIO" : "NI",
+    "NPR" : "NP",
+    "NZD" : "NZ",
+    "OMR" : "OM",
+    "PAB" : "PA",
+    "PEN" : "PE",
+    "PGK" : "PG",
+    "PHP" : "PH",
+    "PKR" : "PK",
+    "PLN" : "PL",
+    "PYG" : "PY",
+    "QAR" : "QA",
+    "RON" : "RO",
+    "RSD" : "RS",
+    "RUB" : "RU",
+    "RWF" : "RW",
+    "SAR" : "SA",
+    "SBD" : "SB",
+    "SCR" : "SC",
+    "SDG" : "SD",
+    "SEK" : "SE",
+    "SGD" : "SG",
+    "SKK" : "SK",
+    "SLL" : "SL",
+    "SOS" : "SO",
+    "SRD" : "SR",
+    "STD" : "ST",
+    "SVC" : "SV",
+    "SYP" : "SY",
+    "SZL" : "SZ",
+    "THB" : "TH",
+    "TJS" : "TJ",
+    "TMT" : "TM",
+    "TND" : "TN",
+    "TOP" : "TO",
+    "TRY" : "TR",
+    "TTD" : "TT",
+    "TWD" : "TW",
+    "TZS" : "TZ",
+    "UAH" : "UA",
+    "UGX" : "UG",
+    "USD" : "US",
+    "UYU" : "UY",
+    "UZS" : "UZ",
+    "VEF" : "VE",
+    "VND" : "VN",
+    "VUV" : "VU",
+    "YER" : "YE",
+    "ZAR" : "ZA",
+    "ZMK" : "ZM",
+    "ZWD" : "ZW"
+}
+
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+ 
+  const ccode =country_list[currency_one]
+  const ccode2 =country_list[currency_two]
+
+
+  document.getElementById("flagimg").src = `https://flagcdn.com/w80/${ccode.toLowerCase()}.png`;
+  document.getElementById("flagimg2").src = `https://flagcdn.com/w80/${ccode2.toLowerCase()}.png`;
+
+  //fetching exchange rate from  ExchangeRate Api
+
+  fetch(`https://v6.exchangerate-api.com/v6/6a6de1ef74c88edaf72ef8bf/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+        const rate = data.conversion_rates[currency_two];
+        rateEl.innerText = ` Rate:  1 ${currency_one} = ${rate} ${currency_two}`;
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
+
+// Event listening
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+
+calculate();
